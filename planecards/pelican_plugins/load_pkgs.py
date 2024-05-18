187f9748cefd83bebebfe7d55a2843b59db6154a
Abdullah Norozi Iranzad <a1norozi@gmail.com>
2024-05-17 23:17:43 -0600

    update load pkgs function to generate card markdown files for each repository file

--- planecards/pelican_plugins/load_pkgs.py
+++ planecards/pelican_plugins/load_pkgs.py
@@ -12,8 +12,32 @@ def add_repository_yaml_data(generator):
     for pkg in pkg_list:
         data_list.append(repository.load_plane_card_yaml(pkg))
 
+    for item in data_list:
+        item_name = item.pc.metadata.name
+        item_version = item.pc.metadata.version
+        item_name_version = item_name.lower()+'_v'+str(item_version)
+        content_dir = os.path.join('content/pages', item_name_version)
+        os.makedirs(content_dir, exist_ok=True)
+        content_file_model = os.path.join(content_dir, 'model.md')
+        with open(content_file_model, 'w') as f:
+            f.write(f"Title: {item_name}\n")
+            f.write(f"URL: {item_name_version+'/model'}\n")
+            f.write(f"save_as: {item_name_version+'/model.html'}\n")
+            f.write(f"template: {'card_model'}\n")
+            f.write(f"model_name: {item_name}\n")
+            f.write(f"model_version: {item_version}\n\n")
+        content_file_umr = os.path.join(content_dir, 'umr.md')
+        with open(content_file_umr, 'w') as f:
+            f.write(f"Title: {item_name}\n")
+            f.write(f"URL: {item_name_version+'/umr'}\n")
+            f.write(f"save_as: {item_name_version+'/umr.html'}\n")
+            f.write(f"template: {'card_umr'}\n")
+            f.write(f"model_name: {item_name}\n")
+            f.write(f"model_version: {item_version}\n\n")
+
     # Add the data list to the context
     generator.context['repository_yaml_data'] = data_list
+    # generator.context['repository'] = repository
 
 def register():
     signals.generator_init.connect(add_repository_yaml_data)
